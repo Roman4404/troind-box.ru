1241a4ab5db5cbcb3897743795d909b3ac90b738
R4404 <r.v.lenshin@gmail.com>
2025-03-11 16:28:15 +0700

    Add home and Yandex Music widget

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+
+app = flask.Flask(__name__)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return flask.render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run()
