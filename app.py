a3ab2271e85231b3f576d8bee6c59749716dbd75
R4404 <r.v.lenshin@gmail.com>
2025-03-25 22:46:21 +0700

    Update home

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = flask.Flask(__name__)
 def hello_world():  # put application's code here
     return flask.render_template('home.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def about():
+    return flask.render_template('login_form.html')
 
 if __name__ == '__main__':
     app.run()
