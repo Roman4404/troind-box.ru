ede9c79a3a49c0458cfb003aebf5eb7634f7ca26
Roman Lenshin <r.v.lenshin@gmail.com>
2025-03-26 17:12:36 +0700

    add form registor

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ def hello_world():  # put application's code here
     return flask.render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def about():
+def login():
     return flask.render_template('login_form.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if flask.request.method == 'GET':
+        return flask.render_template('register_form.html')
+    elif flask.request.method == 'POST':
+        file = flask.request.form['email']
+        print(file)
+        return flask.redirect('/')
 
 if __name__ == '__main__':
     app.run()
